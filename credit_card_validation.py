7fb9b7d677a6357fa132e8d6245a1fd8716507f5
gokulkannaniprimed <52147987+gokulkannaniprimed@users.noreply.github.com>
2019-07-12 16:13:52 +0530

    Add files via upload

--- credit_card_validation.py
+++ credit_card_validation.py
@@ -1,7 +1,7 @@
 import re
 
 list1=[]
-card_number="4557254558855555"
+card_number="4455667767495369"
 matcher1=re.search("[456]\d{3}\d{4}\d{4}\d{4}",card_number)
 matcher2=re.search("^[456]\d{3}-\d{4}-\d{4}-\d{4}",card_number)
 k=0
@@ -10,13 +10,13 @@ if matcher1!=None or matcher2!=None:
         if i!='-':
             list1.append(i)
     count=0
-    for i in range(1,12):
-        if int(list1[i])==int(list1[i-1])+1:
-            count+=1
-        if count==4:
-            k=1
-            print('Invalid')
-            break
+    for i in range(4,16):
+        if int(list1[i])==int(list1[i-1]):
+            if int(list1[i])==int(list1[i-2]):
+                if int(list1[i])==int(list1[i-3]):
+                    print('Invalid')
+                    k=1
+                    break
 else:
     k=1
     print('Invalid')
